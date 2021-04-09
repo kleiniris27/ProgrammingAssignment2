63d1b7d2c395bdbf7ba6bafc85ca7d4dfb247c27
kleiniris27 <81978117+kleiniris27@users.noreply.github.com>
2021-04-09 22:20:22 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,118 +1,30 @@
-makeCacheMatrix <- function(mat = matrix()) {
-    # initializing the inverse to be NULL
-    inv <- NULL
-    
-    # the setter for the matrix
-    set <- function(mtrx) {
-        mat <<- mtrx
-        inv <<- NULL
+# the purpose of this code is that it caches and computes the values of the matrix whether is is invertible matrix or simply inverse
+
+
+makeCacheMatrix <- function(mtx = matrix()) {
+    inverse <- NULL
+    set <- function(x) {
+        mtx <<- x;
+        inverse <<- NULL;
     }
-    
-    # the getter for the matrix
-    get <- function() mat
-    
-    # the setter of the inverse
-    set.inverse <- function(setinv) inv <<- setinv
-
-makeCacheMatrix <- function(mat = matrix()) {
-    # initializing the inverse to be null
-    inv <- NULL
-    
-    # the setter for the matrix
-    set <- function(mtrx) {
-        mat <<- mtrx
-        inv <<- NULL
-    }
-    
-    # the getter for the matrix
-    get <- function() mat
-    
-    # the setter for the matrix
-    set.inverse <- function(setinv) inv <<- setinv
-## created the matrix with a getter and a setter
-
-makeCacheMatrix <- function(mat = matrix()) {
-    # initializing the inverse to be null
-    inv <- NULL
-    
-    # the setter for the matrix
-    set <- function(mtrx) {
-        mat <<- mtrx
-        inv <<- NULL
-    }
-    
-    # the getter for the matrix
-    get <- function() mat
-    
-    # the setter for inverse
-    set.inverse <- function(setinv) inv <<- setinv
-
-makeCacheMatrix <- function(mat = matrix()) {
-    # initializing the inverse to be null
-    inv <- NULL
-    
-    # the setter for the matrix
-    set <- function(mtrx) {
-        mat <<- mtrx
-        inv <<- NULL
-    }
-    
-    # the getter for the matrix
-    get <- function() mat
-    
-    # the setter for inverse
-    set.inverse <- function(setinv) inv <<- setinv
-
-    # the getter for inverse
-    get.inverse <- function() inv
-
-    # the list of the setter and getter functions
-    list(set = set, get = get,
-         set.inverse = set.inverse,
-         get.inverse = get.inverse)
+    get <- function() return(mtx);
+    setinv <- function(inverse) inverse <<- inv;
+    getinv <- function() return(inverse);
+    return(list(get = get, set = set, getinv = getinv, setinv = setinv))
 }
-## the cache solved the inverse of the matrix
-## and if it is already calculated the returns cached
-## you may use results instead
-cacheSolve <- function(cached.mat, ...) {
-    # gets the value of the stored inverse
-    inv <- cached.mat$get.inverse()
-    # if it exists then return it
-    if(!is.null(inv)) {
-        message("getting cached inverse")
+
+# this section is for determining the inverse of the matrix by computing to get the values
+# take note that if the inverse is already calculated, therefore, the cache from inverse must be reclaimed
+
+cacheSolve <- function(mtx, ...) {
+    inverse <- mtx$getinverse()
+    if(!is.null(inverse)) {
+        message("Getting the cached data...")
         return(inv)
-    }
    
-}
-## the cache solved the inverse of the matrix
-## and if it is already calculated the returns cached
-## you may use results instead
-cacheSolve <- function(cached.mat, ...) {
-    # gets the value of the stored inverse
-    inv <- cached.mat$get.inverse()
-    # if it exists then return it
-    if(!is.null(inv)) {
-        message("getting cached inverse")
-        return(inv)
-    }
-  
-}
-## the cache solved the inverse of the matrix
-## and if it is already calculated the returns cached
-## you may use results instead
-cacheSolve <- function(cached.mat, ...) {
-    # gets the value of the stored inverse
-    inv <- cached.mat$get.inverse()
-    # if it exists then return it
-    if(!is.null(inv)) {
-        message("getting cached inverse")
-        return(inv)
-    }
-        
-    raw.mat <- cached.mat$get()
-    inv <- solve(raw.mat, ...)
-    cached.mat$set.inverse(inv)
-
-    # the it returns the inverse
-    inv
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setinverse(inv)
+	inverse
 }
